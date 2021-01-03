3216501b104e4f84cc991ae2c0567d9e06bf6e73
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2021-01-03 01:44:45 -0500

    Remove vestigial state parameter in parser internals

--- Blang/Parser.fs
+++ Blang/Parser.fs
@@ -14,26 +14,26 @@ let private createError errType position =
 let private ( >>= ) a b = Result.bind b a
 let private ( <!> ) a b = Result.map b a
 
-let rec private parseExpr (producerState, produceToken: 'a -> Result<Token * 'a, EvalError>) exprBodyAcc depth =
+let rec private parseExpr (producerState, produceToken: 'a -> Result<Token * 'a, EvalError>) exprBodyAcc =
     producerState |> produceToken
     >>= fun (token, state) ->
             match token.Type with
             | EOF -> Error (createError ExpectedValue token.Position)
             | RParen -> Ok (List.rev exprBodyAcc |> Expression, state)
             | _ -> 
-                parseVal token (state, produceToken) depth 
+                parseVal token (state, produceToken)
                 >>= fun (value, state) -> 
-                        parseExpr (state, produceToken) (value :: exprBodyAcc) depth
-and private parseVal token (producerState, produceToken) depth =
+                        parseExpr (state, produceToken) (value :: exprBodyAcc)
+and private parseVal token (producerState, produceToken) =
     match token.Type with
     | EOF -> (unitValue, producerState) |> Ok
     | Number x -> (NumberAtom x, producerState) |> Ok
     | String x -> (StringAtom x, producerState) |> Ok
     | Symbol x -> (SymbolAtom x, producerState) |> Ok
-    | LParen -> parseExpr (producerState, produceToken) [] (depth + 1)
+    | LParen -> parseExpr (producerState, produceToken) []
     | _ -> Error (createError (UnexpectedToken token.Type) token.Position)
 
 let parse (tokenizerStartState: 'a, tokenProducer: TokenProducer<'a>) : Result<Value * 'a, EvalError> = 
     tokenProducer tokenizerStartState
     >>= fun (firstToken, tokenizerState) ->
-            parseVal firstToken (tokenizerState, tokenProducer) 0
+            parseVal firstToken (tokenizerState, tokenProducer)
