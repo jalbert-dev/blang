e30eae1b56c6e8b61a913700a2327fb951003008
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2021-01-09 19:08:47 -0500

    REPL looks good

--- WebBlang/Main.fs
+++ WebBlang/Main.fs
@@ -7,9 +7,17 @@ open Bolero.Templating.Client
 
 open Blang
 
+type LogEntryType =
+    | LogNone of string
+    | LogWarning of string
+    | LogError of string
+    | LogInfo of string
+    | LogEcho of string
+    | LogResult of string
+
 type Model =
     {
-        Output: string list
+        Output: LogEntryType list
 
         EvalEntryValue: string
 
@@ -18,7 +26,7 @@ type Model =
 
 let initModel =
     {
-        Output = ["c"; "b"; "a"]
+        Output = []
         EvalEntryValue = ""
         ReplState = RuntimeCore.coreScope
     }
@@ -37,19 +45,17 @@ let private ( <!> ) a b = Result.map b a
 let private repl input state =
     Parser.parse (Lexer.create input, Lexer.next)
     <!> fun (value, rest) ->
-            let parsedString = sprintf "> %s" (input.Substring(0, rest.Index))
+            let parsedString = [LogEcho <| input.Substring(0, rest.Index)]
             if rest |> Lexer.atEof |> not then
                 (value, [
-                    sprintf "WARNING: Input beyond the first value is currently discarded."
-                    sprintf "         (Discarded input: \"%s\")" (rest.Source.Substring(rest.Index))
-                    parsedString
-                ])
+                    LogWarning <| sprintf "Input beyond the first value is currently discarded.\n(Discarded input: \"%s\")" (rest.Source.Substring(rest.Index))
+                ] @ parsedString)
             else
-                value, [parsedString]
+                value, parsedString
     >>= fun (value, msgs) -> 
             Runtime.evaluate state value <!> fun x -> x, msgs
     <!> fun ((value, scope), msgs) ->
-            scope, msgs @ [Value.stringify value]
+            scope, msgs @ [LogResult <| Value.stringify value]
 
 let update message model =
     match message with
@@ -60,33 +66,50 @@ let update message model =
         if model.EvalEntryValue.Length > 0 then
             match repl model.EvalEntryValue model.ReplState with
             | Ok (nextState, msgs) ->
-                { model with Output = model.Output @ [String.concat "\n" msgs]
+                { model with Output = model.Output @ msgs
                              ReplState = nextState
                              EvalEntryValue = "" }
             | Error err ->
-                { model with Output = model.Output @ [sprintf "%A" err]
+                { model with Output = model.Output @ [LogError <| sprintf "%A" err]
                              EvalEntryValue = "" }
         else
             model
     | ClearLog ->
         { model with Output = [] }
 
+let formatLogEntry (msg: LogEntryType) =
+    let createEntry (headerClass, headerText) (entryClass, entryText) = [
+        td [attr.``class`` ("eval-log-entry-type has-text-centered " + headerClass)] [text headerText]
+        td [attr.``class`` ("eval-log-entry " + entryClass)] [text entryText]
+    ]
+    let createEntryWithIcon (headerClass, iconClass) (entryClass, entryText) = [
+        td [attr.``class`` ("eval-log-entry-type has-text-centered " + headerClass)] 
+            [i [attr.``class`` iconClass] []]
+        td [attr.``class`` ("eval-log-entry " + entryClass)] [text entryText]
+    ]
+    tr [] (
+        match msg with
+        | LogEcho msg -> createEntry ("eval-log-entry-echo has-text-grey", "\ue0b1") ("eval-log-entry-echo has-text-grey", msg)
+        | LogError msg -> createEntryWithIcon ("eval-log-entry-error has-background-danger-light", "fas fa-times has-text-danger") ("", msg)
+        | LogInfo msg -> createEntryWithIcon ("eval-log-entry-info has-background-info-light", "fas fa-question has-text-info") ("", msg)
+        | LogNone msg -> createEntry ("eval-log-entry-none", "") ("", msg)
+        | LogResult msg -> createEntry ("eval-log-entry-result", "\u2570\u2500") ("has-text-weight-bold", msg)
+        | LogWarning msg -> createEntryWithIcon ("eval-log-entry-warning has-background-warning-light", "fas fa-exclamation has-text-warning") ("", msg)
+    )
+
 let view model dispatch =
     div [attr.``class`` "pb-2 pt-1 pl-2 pr-2 code-text"] [
-        div [attr.``class`` "eval-log-container"] [
+        div [attr.``class`` "eval-log-container sensible-font-size"] [
             table [attr.``class`` "m-0 table is-hoverable is-bordered is-narrow is-fullwidth scrolling-table"] [
                 tbody [] [
-                    forEach model.Output <| fun line ->
-                        tr [] [
-                            td [attr.``class`` "eval-log-entry"] [text line]
-                        ]
+                    forEach model.Output <| formatLogEntry
                 ]
             ]
         ]
         div [attr.id "eval-box-container"] [
             div [attr.``class`` "field pt-2 has-addons"] [
                 div [attr.``class`` "control is-expanded"] [
-                    input [attr.``class`` "input code-text is-dark"
+                    input [attr.``class`` "input code-text is-dark sensible-font-size"
                            attr.``type`` "text"
                            attr.``placeholder`` "Type expression and press Evaluate"
                            bind.input.string model.EvalEntryValue (dispatch << SetEvalEntryField)
@@ -96,13 +119,13 @@ let view model dispatch =
                 ]
                 div [attr.``class`` "control"] [
                     button [
-                        attr.``class`` "button is-dark is-outlined"
+                        attr.``class`` "button is-dark is-outlined sensible-font-size"
                         on.click <| fun _ -> dispatch EvalImmediate
                     ] [text "Evaluate"]
                 ]
                 div [attr.``class`` "control"] [
                     button [
-                        attr.``class`` "button is-dark is-outlined"
+                        attr.``class`` "button is-dark is-outlined sensible-font-size"
                         on.click <| fun _ -> dispatch ClearLog
                     ] [text "Clear"]
                 ]
