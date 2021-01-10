5d3a990d45372558262bc67e2597f8f9591695f5
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2021-01-09 23:30:38 -0500

    CodeMirror writes to text area on change

--- WebBlang/wwwroot/interop.js
+++ WebBlang/wwwroot/interop.js
@@ -1,5 +1,9 @@
 let IDE = null;
 
+function updateTextArea(instance, _) {
+    instance.save();
+}
+
 function createIde() {
     IDE = CodeMirror.fromTextArea(
         document.getElementById("editor-textarea"),
@@ -10,6 +14,8 @@ function createIde() {
     IDE.getWrapperElement().classList.add("editor-wrapper");
     IDE.getWrapperElement().classList.add("ctrl-inside-bordered");
     IDE.getWrapperElement().classList.add("sensible-font-size");
+
+    IDE.on("changes", updateTextArea);
 
     let loadingText = document.getElementById("editor-loading");
     loadingText.setAttribute("style", "display: none;");
