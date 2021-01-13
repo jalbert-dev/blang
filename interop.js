01922fedce4c4960b1b08b7d1bb5eae9e13db8c0
jalbert-dev <47063257+jalbert-dev@users.noreply.github.com>
2021-01-13 03:49:13 +0000

    Deploying to gh-pages from main @ 71f3ff034236e8cb9f67360f0a09612113b5e314 🚀

---
+++ interop.js
@@ -0,0 +1,57 @@
+let IDE = null;
+
+function updateTextArea(instance, _) {
+    instance.save();
+}
+
+function createIde() {
+    IDE = CodeMirror.fromTextArea(
+        document.getElementById("editor-textarea"),
+        {
+            lineNumbers: true
+        });
+
+    IDE.getWrapperElement().classList.add("editor-wrapper");
+    IDE.getWrapperElement().classList.add("ctrl-inside-bordered");
+    IDE.getWrapperElement().classList.add("sensible-font-size");
+
+    IDE.on("changes", updateTextArea);
+
+    let loadingText = document.getElementById("editor-loading");
+    loadingText.setAttribute("style", "display: none;");
+
+    Split([".editor-container", ".repl-container"], { 
+        minSize: 0,
+        gutter: (index, direction) => {
+            const gutter = document.createElement('div')
+            gutter.id = "main-splitter"
+            gutter.className = `gutter gutter-${direction}`
+            return gutter
+        },
+        gutterSize: 8,
+        minSize: 240,
+        elementStyle: (_, elementSize, gutterSize, index) => {
+            if (index === 0) {
+                // "don't do anything besides return the style object in these 
+                //  functions. Both of these functions should be pure"
+                // yeah, sorry SplitJS guy, this splitter ain't gonna move itself
+                // (not with a janky "position: absolute" based layout that is!)
+                let splitter = document.getElementById("main-splitter");
+                if (splitter) {
+                    splitter.setAttribute("style", `left: calc(${elementSize}% - ${gutterSize}px);`);
+                }
+
+                return { 'right': `calc(${100 - elementSize}% + ${gutterSize}px)` }
+            }
+            else {
+                return { 'left': `calc(${100 - elementSize}% + ${gutterSize}px)` }
+            }
+        },
+        gutterStyle: (_, gutterSize, __) => {
+            console.log(gutterSize)
+            return {
+                'width': '8px'
+            }
+        }
+    });
+}
