59859c7f31a8f38052662ba75664c34bb7503cdb
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2021-01-14 21:33:04 -0500

    Stack trace works, but is very, VERY ugly

--- Blang/Evaluator.fs
+++ Blang/Evaluator.fs
@@ -7,13 +7,18 @@ open Blang.EvalUtil
 let private ( >>= ) a b = Result.bind b a
 let private ( <!> ) a b = Result.map b a
 let private ( >>! ) a b = Result.mapError b a
+let private ( <*> ) f x = 
+    match (f, x) with
+    | Ok f, Ok x -> Ok <| f x
+    | Error err, _ -> Error err
+    | _, Error err -> Error err
 
 type NativeFunc = Scope -> Value list -> Result<ValueDef * SideEffect list, EvalError>
 type NativeFuncMap = Map<string, NativeFunc>
 
 type private ExecDef =
     | NativeCall of NativeFunc * Value list * Scope
-    | UserCall of Value list * Scope
+    //| UserCall of Value list * Scope
 
 let (|ExprValueType|_|) = function
     | { Value.Type = Expression x } -> Some x
@@ -30,7 +35,7 @@ let private expectFunctionIdentifier value =
     expectSymbol value >>! fun _ -> { EvalError.Type = FunctionIdentifierMustBeSymbol value.Type
                                       Position = value.Position }
 
-let wrapFuncEvalError fname pos err =
+let private wrapFuncEvalError fname pos err =
     { EvalError.Type = ErrorEvaluatingFunction (fname, err)
       Position = pos }
 
@@ -53,8 +58,8 @@ let ( >=> ) f g x = f x >>= g
 // Evaluates a list of values sequentially within the given scope, updating the scope according
 // to any side effects returned while evaluating each. Returns a Result containing
 // either a tuple of the list of evaluated values and the final scope, or an EvalError.
-let private evaluateValueListWithSideEffects evaluator startScope valueList =
-    let f value (acc, scope) = evaluator scope value <!> mapPair (prependTo acc, applySideEffects scope)
+let private evaluateValueListWithSideEffects evaluator stackTrace startScope valueList =
+    let f value (acc, scope) = evaluator stackTrace scope value <!> mapPair (prependTo acc, applySideEffects scope)
     // side effects must be applied in list order
     List.fold (fun s v -> s >>= f v) <| Ok ([], startScope) <| valueList
     // that means the result list will be backwards due to prepend/cons!
@@ -63,59 +68,75 @@ let private evaluateValueListWithSideEffects evaluator startScope valueList =
 // Evaluates a given value inside the given scope and applies any side effects to
 // the scope. Returns a Result containing either a tuple of the evaluated value
 // and the updated scope, or an EvalError.
-let private evaluateValueSingleWithSideEffects evaluator startScope value =
-    evaluateValueListWithSideEffects evaluator startScope [value]
+let private evaluateValueSingleWithSideEffects evaluator stackTrace startScope value =
+    evaluateValueListWithSideEffects evaluator stackTrace startScope [value]
     <!> mapPair (List.exactlyOne, id)
 
-// Helper function to parse function definitions and run the corresponding executors.
-let private evaluateFunctionWithDef (nativeFuncs: NativeFuncMap)
-                                    evaluator
-                                    functionStartPosition
-                                    functionIdentifierPosition
-                                    args 
-                                    (funcDef, evaluationScope) =
-    let evaluator = evaluator nativeFuncs
+// Helper function to parse function definitions and return any
+// information needed to perform the function call.
+let private prepareFuncCall nativeFuncLookup
+                            (evaluator: Value list -> Scope -> Value -> Result<Value * SideEffect list, EvalError * Value list>)
+                            stackTrace
+                            args 
+                            (funcDef, evaluationScope) =
     match funcDef with
     | NativeFuncDef identifier ->
+        let ( <!> ) = Result.map
         let evalArgs () =
             // For now, quote function is special-cased to not evaluate its args
             if identifier = "'" then
                 Ok (args, evaluationScope)
             else
-                evaluateValueListWithSideEffects evaluator evaluationScope args
-
-        let execNativeFunc f =
-            evalArgs ()
-            >>= fun (args, scope) -> f scope args
-            >>! wrapFuncEvalError identifier functionStartPosition
+                evaluateValueListWithSideEffects evaluator stackTrace evaluationScope args
         
-        identifier
-        |> lookupNativeFunc nativeFuncs functionIdentifierPosition
-        >>= execNativeFunc
-    | _ -> Error { EvalError.Type = InvalidFunctionDefinition funcDef.Type
-                   Position = None}
+        fun f (args, scope) -> NativeCall (f, args, scope)
+        <!> nativeFuncLookup identifier
+        <*> evalArgs ()
+        
+    | _ -> Error ({ EvalError.Type = InvalidFunctionDefinition funcDef.Type
+                    Position = None}, [])
 
 /// Evaluates the given value within the given scope, and returns a Result
 /// containing either a tuple of the resulting value and a list of side effects,
 /// or an EvalError.
 /// 
 /// Returned SideEffects are to be applied by the caller.
-let rec evaluateValue (nativeFuncs: NativeFuncMap) 
-                 (scope: Scope) 
-                 (value: Value) 
-                 : Result<Value * SideEffect list, EvalError> =
-    // Expressions need to be evaluated as a function, Symbols need to be looked up, everything else passes through
-    match value.Type with
-    | SymbolAtom _ -> lookupSymbolValue scope value <!> fun x -> x, []
-    | Expression [] -> Ok (Parser.unitValue, [])
-    | Expression (funcIdent::args) ->
-        let evaluateFunctionWithDef' = evaluateFunctionWithDef nativeFuncs evaluateValue value.Position
-        let evaluateValueSingleWithSideEffects' = evaluateValueSingleWithSideEffects (evaluateValue nativeFuncs) scope
+let evaluateValue (nativeFuncs: NativeFuncMap)
+                  (rootScope: Scope)
+                  (rootValue: Value)
+                  : Result<Value * SideEffect list, EvalError> =
+    let rec loop stackTrace scope (value: Value) : Result<Value * SideEffect list, EvalError * Value list>  =
+        // Expressions need to be evaluated as a function, Symbols need to be looked up, everything else passes through
+        match value.Type with
+        | SymbolAtom _ -> lookupSymbolValue scope value >>! (fun x -> x, stackTrace) <!> fun x -> x, []
+        | Expression [] -> Ok (Parser.unitValue, [])
+        | Expression (funcIdent::args) ->
+            let stackTrace = funcIdent :: stackTrace
+            let nativeFuncLookup x = lookupNativeFunc nativeFuncs value.Position x >>! (fun x -> x, stackTrace)
+            let prepareFuncCall' = prepareFuncCall nativeFuncLookup loop stackTrace
+            let evaluateValueSingleWithSideEffects' = evaluateValueSingleWithSideEffects loop stackTrace scope
 
-        evaluateValueSingleWithSideEffects' funcIdent
-        >>= evaluateFunctionWithDef' funcIdent.Position args
-        >>= function
-            | (Immediate value, sideEffects) -> Ok (value, sideEffects)
-            | (NeedsEval (toEval, scope), []) -> evaluateValue nativeFuncs scope toEval
-            | (NeedsEval _, _) -> failwith "Native functions can't return values needing evaluation AND side effects!!"
-    | _ -> Ok (value, [])
+            evaluateValueSingleWithSideEffects' funcIdent
+            >>= prepareFuncCall' args
+            >>= function
+                | NativeCall (f, evaledArgs, scope) -> 
+                    f scope evaledArgs >>! (fun x -> x, stackTrace)
+            >>= function
+                | (Immediate value, sideEffects) -> Ok (value, sideEffects)
+                | (NeedsEval (toEval, scope), []) -> loop stackTrace scope toEval
+                | (NeedsEval _, _) -> failwith "Functions can't return values needing evaluation AND side effects!!"
+        | _ -> Ok (value, [])
+    let flattenStackTrace (terminalError, stackTrace) =
+        let getStackInfo = function
+            | { Value.Type = StringAtom x; Position = y } -> x, y
+            | { Value.Type = SymbolAtom x; Position = y } -> x, y
+            | _ -> "[invalid stack trace]", None
+        let mutable rv = terminalError
+        for err in stackTrace do
+            let (msg, pos) = getStackInfo err
+            rv <- { EvalError.Type = ErrorEvaluatingFunction (msg, rv)
+                    Position = pos }
+        rv
+
+    loop [] rootScope rootValue 
+    >>! flattenStackTrace
